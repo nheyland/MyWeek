50b090986bd25af229c7dda6f1ba8f7f8574c102
Nick Heyland <nheyland@gmail.com>
2021-02-24 17:11:56 -0500

    adding intial project

---
+++ login/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Lets put the users information here
+# lets connect the users profile to an event in the planner models
+# lets connect events by inviting users via a dropdown menu of users to share with -> sends an invite
+
+
+class UserManager(models.Manager):
+    pass
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    objects = UserManager()
