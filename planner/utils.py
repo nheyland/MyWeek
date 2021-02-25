1ca1b78bd773ef37d72772602ad04259a840a594
Nick Heyland <nheyland@gmail.com>
2021-02-24 23:19:01 -0500

    adding styles to force size of day boxes

--- planner/utils.py
+++ planner/utils.py
@@ -14,7 +14,8 @@ class Calendar(HTMLCalendar):
         events_per_day = events.filter(start_time__day=day)
         d = ''
         for event in events_per_day:
-            d += f'<li class="calendar_list"> {event.get_html_url} </li>'
+            d += f'<li class="calendar_list"> <a href="details/{event.id}">{event.title}</a> </li>'
+
         if day != 0:
             return f"<td><span class='date'>{day}</span><ul> {d} </ul></td>"
         return '<td></td>'
