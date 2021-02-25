5f9896230ec2ab02721e2a6e8dbcdb4570a7587b
Johnny <jaoldaker1@gmail.com>
2021-02-25 18:49:04 -0500

    Added delete and update event functions. Modified planner.html for testing. Added default value input to views.planner(). This allows the events to be edited on the same page as adding the events

--- planner/urls.py
+++ planner/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('planner/', views.planner),
-    path('create_event/', views.create_event)
+    path('create_event/', views.create_event),
+    path('update_event/<int:event_to_update>', views.planner),
+    path('process_update/<int:event_id>', views.update_event)
+
 ]
--- planner/views.py
+++ planner/views.py
@@ -4,18 +4,24 @@ from .models import Event
 from login.models import User
 
 
-def planner(request):
+def planner(request,event_to_update=None):
     if not 'user_id' in request.session.keys():
         return redirect('/')
+    if event_to_update:
+        event_to_update = Event.objects.get(id=event_to_update)
     context = {
-        'events': Event.objects.all()}
+        'events': Event.objects.all(),
+        'event_to_update':event_to_update}
     return render(request, 'planner.html', context)
 
 
 def create_event(request):
-    if not 'user_id' in request.session.keys():
+    if 'user_id' not in request.session.keys():
         return redirect('/')
     x = request.POST
+    print(x['start_time'])
+    print(x['end_time'])
+    print(x['date'])
     Event.objects.create(
         created_by=User.objects.get(id=request.session['user_id']),
         title=x['title'],
@@ -26,3 +32,24 @@ def create_event(request):
         public=x['public']
     )
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+def delete_event(request):
+    if 'user_id' not in request.session.keys():
+        return redirect('/')
+    x = request.POST
+    Event.objects.filter(id=request.POST['event_id']).delete()
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+def update_event(request, event_id):
+    if 'user_id' not in request.session.keys():
+        return redirect('/')
+    x = request.POST
+    event_to_update = Event.objects.get(id=event_id)
+    event_to_update.title = x['title']
+    event_to_update.desc = x['desc']
+    event_to_update.date = x['date']
+    event_to_update.start_time = x['start_time']
+    event_to_update.end_time = x['end_time']
+    event_to_update.public = x['public']
+    event_to_update.save()
+    return redirect('/planner/')
