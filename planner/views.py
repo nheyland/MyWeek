109c7d992b5bd817cc926b218a30688cd667e958
Nick Heyland <nheyland@gmail.com>
2021-02-25 14:42:00 -0500

    fixing db

--- planner/views.py
+++ planner/views.py
@@ -18,13 +18,13 @@ def planner(request):
     if not 'user_id' in request.session.keys():
         return redirect('/')
     context = {
-        'events': Event.objects.all()}
+        'events': Event.objects.all()
+    }
     # Month
     d = get_date(request.GET.get('day', None))
     cal = Calendar(d.year, d.month)
     cal.setfirstweekday(6)
     context['cal'] = cal.whole_month(withyear=True)
-
     context['week'] = cal.whole_week(d.day)
 
     return render(request, 'planner.html', context)
@@ -38,7 +38,6 @@ def create_event(request):
         created_by=User.objects.get(id=request.session['user_id']),
         title=x['title'],
         desc=x['desc'],
-        date=x['date'],
         start_time=x['start_time'],
         end_time=x['end_time'],
         public=x['public'],
