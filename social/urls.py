c3d9186048fe154481c1ebe43a4160a2bc19c643
Erin Winking <ewink@yahoo.com>
2021-02-26 00:13:10 -0800

    Basic function of #4 Friends List done.

--- social/urls.py
+++ social/urls.py
@@ -3,5 +3,6 @@ import social.views as SocialViews
 
 urlpatterns = [
     path('<int:userID>', SocialViews.viewProfile, name = 'viewProfile'),
+    path('addFriend/', SocialViews.addFriend, name = 'addFriend'),
     path('allusers/', SocialViews.allUsers, name = 'allUsers'),
 ]
