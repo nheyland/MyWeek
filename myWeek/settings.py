b3934bb62107193ddcf4e55f99bbf7549082c8f1
Erin Winking <ewink@yahoo.com>
2021-02-26 00:16:57 -0800

    One small change. Still focused on #4.

--- myWeek/settings.py
+++ myWeek/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+# ADDED THIS BACK IN FOR SIMPLICITY AND WHO CARES IF IT'S STOLE, RIGHT? /ew
+SECRET_KEY = "***REMOVED***"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
