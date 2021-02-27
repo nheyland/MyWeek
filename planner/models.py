9e29d5beaeb68c1046fa18f4d785e80dde9a8860
Johnny <jaoldaker1@gmail.com>
2021-02-26 19:23:25 -0500

    added validator that checks for timing conflicts and title/ description length

--- planner/models.py
+++ planner/models.py
@@ -1,9 +1,26 @@
 from django.db import models
 from login.models import User
-
-
+import datetime
+import pytz
 class EventManager(models.Manager):
-    pass
+    def validations(self, postData, userID):\
+        errors = {}
+
+        user = User.objects.get(id=userID)
+        new_start = datetime.datetime.fromisoformat(postData['start_time'])
+        new_end = datetime.datetime.fromisoformat(postData['end_time'])#puts postdata times into datetime
+
+        new_start = pytz.utc.localize(new_start)
+        new_end = pytz.utc.localize(new_end)# adds utc time zone
+        for event in user.created_event.all():
+            if (new_start >= event.start_time or 
+                new_end <= event.end_time):
+                errors['conflict '] = "You have a sceduling conflict with"+event.title
+        if length(postData['title']) < 3:
+            errors['title'] = "Title must be longer than three characters."
+        if length(postData['description']) < 5:
+            errors['description'] = "Description must be longer than five characters"
+        return error
 
 
 class Event(models.Model):
