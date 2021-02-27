2332b6ec070905b1b263ca1e4b0d566c3ec80dab
Erin Winking <ewink@yahoo.com>
2021-02-27 00:22:27 -0800

    Initial ewink-02 commit. Adding email capabilities.

--- social/views.py
+++ social/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from login.models import User
+# vv NEEDED FOR EMAILING. vv
+from django.core.mail import send_mail
 
 # VIEW USER PROFILE
 def viewProfile(request, userID):
@@ -22,10 +24,24 @@ def allUsers(request):
 def addFriend(request):
     # Making Assumptions till the User Profile is built.
     currentUser = User.objects.get(id = request.session['user_id'])
-    # userProfile = UserProfile.objects.get(user = currentUser)
     friend2Add = User.objects.get(id = request.POST['friendID'])
-    # friendsProfile = UserProfile.objects.get(user = friend2Add)
     currentUser.friends.add(friend2Add)
-    # It should be reciprocal
-    friend2Add.friends.add(currentUser)
+    # REMOVED FRIEND RECIPROCATION. JUST BECAUSE YOU WANT SOMEONE ON YOUR LIST DOESN'T MEAN THEY
+    # WANT YOU ON THEIRS...
+    return redirect('allUsers')
+
+
+# EMAIL YOUR FRIEND AND TELL HIM TO JOIN OUR SITE!!!
+def inviteFriend(request):
+    userSending = User.objects.get(id = request.session['user_id'])
+    subject = f'{userSending.first_name} has invited you to join MyWeek!'
+    message = f"Hi there, {request.POST['invitee_name']}! {userSending.first_name} {userSending.last_name} is planning an event and would like to invite you, but you're not a MyWeek user! \n Sign up to be included in this and future events! http://localhost:8000"
+    invitee = request.POST['invitee_email']
+    send_mail(
+        subject,
+        message,
+        'MyWeek@MyWeek.com',
+        invitee,
+        fail_silently = False,
+    )
     return redirect('allUsers')
