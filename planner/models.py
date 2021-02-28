15ac7696400f75954ec69a8b3d8aa0795586531c
Nick Heyland <nheyland@gmail.com>
2021-02-27 20:36:04 -0500

    s

--- planner/models.py
+++ planner/models.py
@@ -2,26 +2,29 @@ from django.db import models
 from login.models import User
 import datetime
 import pytz
+
+
 class EventManager(models.Manager):
     def validations(self, postData, userID):
         errors = {}
         if postData['start_time'] == '' or postData['end_time'] == '':
             errors['empty_time'] = "Start and end time cannot be emtpy."
-        if length(postData['title']) < 3:
+        if len(postData['title']) < 3:
             errors['title'] = "Title must be longer than three characters."
-        if length(postData['description']) < 5:
+        if len(postData['description']) < 5:
             errors['description'] = "Description must be longer than five characters"
         user = User.objects.get(id=userID)
         new_start = datetime.datetime.fromisoformat(postData['start_time'])
-        new_end = datetime.datetime.fromisoformat(postData['end_time'])#puts postdata times into datetime
+        new_end = datetime.datetime.fromisoformat(
+            postData['end_time'])  # puts postdata times into datetime
 
         new_start = pytz.utc.localize(new_start)
-        new_end = pytz.utc.localize(new_end)# adds utc time zone
+        new_end = pytz.utc.localize(new_end)  # adds utc time zone
         for event in user.created_event.all():
-            if (new_start >= event.start_time or 
-                new_end <= event.end_time):
+            if (new_start >= event.start_time or
+                    new_end <= event.end_time):
                 errors['conflict '] = "You have a sceduling conflict with"+event.title
-        return error
+        return errors
 
 
 class Event(models.Model):
